f4ace080567f2a8c142fb88b354b9b4099104def
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-21 17:53:57 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,12 @@
 class BankAccount
-
+attr_accessor :balance, :transfer
+ 
+  def initialize(balance)
+    @balance = balance
 end
+
+  def initialize(transfer)
+    @transfer = transfer
+  end
+  
+  
